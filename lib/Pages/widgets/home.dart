f7cf2d71305f93832da5c1a0a1ae2b1e322aa59d
Carmine Capone <carminecapone32@gmail.com>
2023-02-28 11:40:47 +0100

    Assets Fix
    Immagini caricate da assets e non internet

--- lib/Pages/widgets/home.dart
+++ lib/Pages/widgets/home.dart
@@ -40,8 +40,7 @@ class _Home extends State<Home> {
                   child: Column(
                     children: [
                       Ink.image(
-                        image: NetworkImage(
-                            "https://scontent-fco2-1.xx.fbcdn.net/v/t39.30808-6/330579137_565243368659554_7562562589408494825_n.png?_nc_cat=103&ccb=1-7&_nc_sid=e3f864&_nc_ohc=mF77DI93Rq8AX8i-05B&tn=kf5h-3b--Mrn-CT3&_nc_ht=scontent-fco2-1.xx&oh=00_AfBRzqn3NCooDKKbJ3qarKUrjxeqhESfn1-n04UG-O1XNw&oe=64023B74"),
+                        image: AssetImage("assets/images/selvetta.png"),
                         height: 150,
                         width: 300,
                         fit: BoxFit.fill,
@@ -74,8 +73,7 @@ class _Home extends State<Home> {
                   child: Column(
                     children: [
                       Ink.image(
-                        image: NetworkImage(
-                            "https://scontent-fco2-1.xx.fbcdn.net/v/t39.30808-6/326055203_502022251994288_896415117840245242_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=e3f864&_nc_ohc=mzrGS0keIjcAX_Hyd0Y&_nc_ht=scontent-fco2-1.xx&oh=00_AfB8vc5BS9HoTNFdX0kfkXMyN-nZsTFSDfB3LzxYXQ74PQ&oe=63FD7D19"),
+                        image: AssetImage("assets/images/sirenetta.jpg"),
                         height: 150,
                         width: 300,
                         fit: BoxFit.fill,
@@ -108,8 +106,7 @@ class _Home extends State<Home> {
                   child: Column(
                     children: [
                       Ink.image(
-                        image: NetworkImage(
-                            "https://scontent-fco2-1.xx.fbcdn.net/v/t31.18172-8/27709703_2008063602764952_8705598066956287630_o.jpg?_nc_cat=103&ccb=1-7&_nc_sid=e3f864&_nc_ohc=0QX1hEkXoP0AX91fjZk&_nc_ht=scontent-fco2-1.xx&oh=00_AfBJ_RoaRSnO0sfjiA5sGuiJgDYnnhkjiuzXOEkujUyaVA&oe=641F1517"),
+                        image: AssetImage("assets/images/baracchino.jpg"),
                         height: 150,
                         width: 300,
                         fit: BoxFit.fill,
@@ -142,8 +139,7 @@ class _Home extends State<Home> {
                   child: Column(
                     children: [
                       Ink.image(
-                        image: NetworkImage(
-                            "https://scontent-fco2-1.xx.fbcdn.net/v/t1.6435-9/192331046_4019291364853079_7857054923546642601_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=e3f864&_nc_ohc=-OxQ3VuObLgAX-PAF4m&_nc_oc=AQmhPYq7ToYA2WWkKXHI3KUm6wS-12jCbbBWQTTL1zXZP4TUbZqxX9BIh7jk3Mbapzc&tn=kf5h-3b--Mrn-CT3&_nc_ht=scontent-fco2-1.xx&oh=00_AfCGC9ypIu8RaYogSFcq61HY61kEl661bEO8Ihf6N27UgA&oe=641F2952"),
+                        image: AssetImage("assets/images/verde.jpg"),
                         height: 150,
                         width: 300,
                         fit: BoxFit.fill,
@@ -176,8 +172,7 @@ class _Home extends State<Home> {
                   child: Column(
                     children: [
                       Ink.image(
-                        image: NetworkImage(
-                            "https://scontent-fco2-1.xx.fbcdn.net/v/t1.6435-9/31543871_2099994430257220_7578342082939453440_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=e3f864&_nc_ohc=oVaqaROtCt8AX-_-OS8&_nc_ht=scontent-fco2-1.xx&oh=00_AfBS4Z03ZnBElwmSA3P9Z99eUKVVbXVESi5Me02liDiZ4w&oe=641F273F"),
+                        image: AssetImage("assets/images/ondeblu.jpg"),
                         height: 150,
                         width: 300,
                         fit: BoxFit.fill,

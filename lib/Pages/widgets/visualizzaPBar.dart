4a6db51346b17cb649e23557504bd711b8697e20
Carmine Capone <carminecapone32@gmail.com>
2023-03-06 20:28:37 +0100

    Prenotazioni Bar Operative

--- lib/Pages/widgets/visualizzaPBar.dart
+++ lib/Pages/widgets/visualizzaPBar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:go2beach/Pages/widgets/sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 final usersRef = FirebaseFirestore.instance.collection('PrenotazioneBar');
 
@@ -30,6 +32,63 @@ class _visualizzaPBar extends State<visualizzaPBar> {
 
   @override
   Widget build(BuildContext context) {
+    var lido = users.map((user) => user['Lido']).toList(growable: false);
+    var data =
+        users.map((user) => user['Data'].toDate()).toList(growable: false);
+    var cocacola =
+        users.map((user) => user['Cocacola']).toList(growable: false);
+    var acqua = users.map((user) => user['Acqua']).toList(growable: false);
+    var spritz = users.map((user) => user['Spritz']).toList(growable: false);
+    var fanta = users.map((user) => user['Fanta']).toList(growable: false);
+    var estathe = users.map((user) => user['Estathe']).toList(growable: false);
+    int? count = lido.length;
+    Widget Prenotazione(int i) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Divider(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Text(
+                " Bar Lido ${lido[i]}",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  ))
+            ],
+          ),
+          Text(
+            " Data: ${data[i]}\n",
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            " Cocacola: ${cocacola[i]}, Acqua: ${acqua[i]}, Estathè: ${estathe[i]}, Fanta: ${fanta[i]}, Spritz: ${spritz[i]} ",
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+            ),
+          ),
+          Divider(
+            height: 20,
+          )
+        ],
+      );
+    }
+
     return Padding(
       padding: const EdgeInsets.only(bottom: 16.0, top: 16),
       child: SingleChildScrollView(
@@ -44,29 +103,7 @@ class _visualizzaPBar extends State<visualizzaPBar> {
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      " Bar Lido ${users.map((user) => user['Lido']).toString().replaceAll((r"("), '').replaceAll((r")"), '')}",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      " Data: ${users.map((user) => user['Data'].toDate()).toString().replaceAll((r"("), '').replaceAll((r")"), '')}\n",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "  Cocacola: ${users.map((user) => user['Cocacola']).toString().replaceAll((r"("), '').replaceAll((r")"), '')}, Acqua: ${users.map((user) => user['Acqua']).toString().replaceAll((r"("), '').replaceAll((r")"), '')}, Estathè: ${users.map((user) => user['Estathe']).toString().replaceAll((r"("), '').replaceAll((r")"), '')}, Fanta: ${users.map((user) => user['Fanta']).toString().replaceAll((r"("), '').replaceAll((r")"), '')}, Spritz: ${users.map((user) => user['Spritz']).toString().replaceAll((r"("), '').replaceAll((r")"), '')} ",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
-                      ),
-                    ),
+                    for (int i = 0; i < count; i++) Prenotazione(i),
                   ]),
             )),
       ),
